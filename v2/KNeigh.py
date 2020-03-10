02cac401a78abe43b5c043b0ee6eb5a24d88f70e
Marco Lorusso <marco.lorusso96@gmail.com>
2020-03-10 15:39:08 +0100

    Added uncorrelated features

--- v2/KNeigh.py
+++ v2/KNeigh.py
@@ -19,6 +19,7 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.svm import SVR
 import xgboost as xgb
 from xgboost import XGBRegressor
+import matplotlib.pyplot as plt
 
 
 seed = 12345
@@ -38,27 +39,29 @@ def create_mlp(dim, regress=False):
 	return model
 
 
-down = 1
+down = 0
 #filename = args.save.replace('.pdf', '') + '_Data.json'
 #file = open(filename, 'wb')
 #np.savetxt(file, ["Calculated Parameters\n"], "%s")
 
-data = pd.read_csv("data.csv",index_col=False)
+
+#### https://drive.google.com/open?id=1pZoeB-J7xMqlkRBNXYEnkug1PZKUmc62  LINK FOR THE DATASET
+data = pd.read_csv("datatot.csv",index_col=False)
 
 if down == 1:
-    data = data.drop(data[data['Track_type'] == 3 ].index)
+    data = data.drop(data[data['hminus_TRACK_Type'] == 3 ].index)
     #Dropping all tracks with Type 3 (long)
 else:
-    data = data.drop(data[data['Track_type'] == 5 ].index)
+    data = data.drop(data[data['hminus_TRACK_Type'] == 5 ].index)
 
 #Declaring real masses from pdg
-data= data.drop(["Track_type"],axis=1)
+# data= data.drop(["Track_type"],axis=1)
 pionm = 139.57061
 protonm = 938.272081
 lambdam = 1115.683
 
 
-variables = ["V0_Endvertex_Z","V0_Endvertex_Y","V0_FDCHI2_ORIVX","V0_M","V0_FD_ORIVX","Lambda_E","hplus_IP_OWNPV","hplus_IPCHI2_OWNPV","hplus_P","hminus_P","Angle","Resolution"]
+variables = ["V0_ENDVERTEX_Z","V0_ENDVERTEX_Y","hplus_P","Angle","nTracks","V0_ORIVX_Z","V0_ORIVX_CHI2","Resolution"]
 
 
 # units = [""]
@@ -82,21 +85,21 @@ variables = ["V0_Endvertex_Z","V0_Endvertex_Y","V0_FDCHI2_ORIVX","V0_M","V0_FD_O
 # units.extend(transverseUnits)
 # units.extend(angleUnits)
 
-Limits=[]
-for var in variables:
-   tmp = data.sort_values(by=var)
-   tmp = tmp.reset_index(drop=True)
-   if down == 1:
-       lms=[[tmp.loc[0, var],tmp.loc[63751, var],tmp.loc[127503, var],tmp.loc[191255, var]]]
-       rng = 40 #range
-   else:
-       lms=[[tmp.loc[0, var],tmp.loc[26852, var],tmp.loc[53706, var],tmp.loc[80558, var]]]
-       rng = 20
-   Limits.extend(lms)
+# Limits=[]
+# for var in variables:
+#    tmp = data.sort_values(by=var)
+#    tmp = tmp.reset_index(drop=True)
+#    if down == 1:
+#        lms=[[tmp.loc[0, var],tmp.loc[63751, var],tmp.loc[127503, var],tmp.loc[191255, var]]]
+#        rng = 40 #range
+#    else:
+#        lms=[[tmp.loc[0, var],tmp.loc[26852, var],tmp.loc[53706, var],tmp.loc[80558, var]]]
+#        rng = 20
+#    Limits.extend(lms)
 
-LowData = pd.DataFrame()
-InterData = pd.DataFrame()
-HighData = pd.DataFrame()
+# LowData = pd.DataFrame()
+# InterData = pd.DataFrame()
+# HighData = pd.DataFrame()
 
 cs = MinMaxScaler()
 
@@ -105,17 +108,17 @@ cs = MinMaxScaler()
 # data = data.div([norm[0],norm[1],norm[2],norm[3],norm[4],norm[5],1])
 
 
-for i in range(len(variables)):
-    LowData = data.drop(data[data["{}".format(variables[i])] >= Limits[i][1]].index)
-    InterData = data.drop(data[(data["{}".format(variables[i])] >= Limits[i][2]) | (data["{}".format(variables[i])] < Limits[i][1])].index)
-    HighData = data.drop(data[(data["{}".format(variables[i])] >= Limits[i][3]) | (data["{}".format(variables[i])] < Limits[i][2])].index)
+# for i in range(len(variables)):
+#     LowData = data.drop(data[data["{}".format(variables[i])] >= Limits[i][1]].index)
+#     InterData = data.drop(data[(data["{}".format(variables[i])] >= Limits[i][2]) | (data["{}".format(variables[i])] < Limits[i][1])].index)
+#     HighData = data.drop(data[(data["{}".format(variables[i])] >= Limits[i][3]) | (data["{}".format(variables[i])] < Limits[i][2])].index)
 
-bst = XGBRegressor(learning_rate=0.15,max_depth=3,colsample_bytree=0.8,n_jobs=-1,n_estimators=100,num_parallel_tree=5,objective='reg:squarederror')
+bst = XGBRegressor(learning_rate=0.1,max_depth=4,n_jobs=-1,n_estimators=150,num_parallel_tree=10,objective='reg:squarederror',subsample=0.8)
 
 
 data = data.dropna()
 
-Xtrain,Xvalid,Ytrain,Yvalid=train_test_split(data[["V0_Endvertex_Z","V0_Endvertex_Y","V0_FDCHI2_ORIVX","V0_M","V0_FD_ORIVX","Lambda_E","hplus_IP_OWNPV","hplus_IPCHI2_OWNPV","hplus_P","hminus_P","Angle"]],data["Resolution"],test_size=0.3)
+Xtrain,Xvalid,Ytrain,Yvalid=train_test_split(data[["V0_ENDVERTEX_Z","V0_ENDVERTEX_Y","hplus_P","Angle","nTracks","V0_ORIVX_Z","V0_ORIVX_CHI2","V0_ORIVX_Y"]],data["Resolution"],test_size=0.3)
 
 Xtrain = cs.fit_transform(Xtrain)
 Xvalid = cs.transform(Xvalid)
@@ -134,15 +137,47 @@ kf = KFold(n_splits=10,shuffle=True,random_state=seed)
 vali = cross_val_score(bst,Xvalid,Yvalid,cv=kf,verbose=1,n_jobs=-1)
 #print(bst.get_params())
 print("####################Xgboost")
-trainbst = bst.fit(Xtrain,Ytrain,eval_set=[(Xtrain, Ytrain), (Xvalid, Yvalid)])
-
+trainbst = bst.fit(Xtrain,Ytrain,eval_set=[(Xtrain, Ytrain), (Xvalid, Yvalid)],eval_metric=['rmse','mae'],verbose=True)
+evres=bst.evals_result() # See MAE metric
 print(vali.mean())
+
+plt.plot(list(evres['validation_0']['rmse']))
+plt.plot(list(evres['validation_1']['rmse']))
+plt.title('Model rmse')
+plt.ylabel('rmse')       
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+#plt.savefig("Keras_NN_Accuracy.png")
+plt.show() 
+plt.clf()
+
+plt.plot(list(evres['validation_0']['mae']))
+plt.plot(list(evres['validation_1']['mae']))
+plt.title('Model mae')
+plt.ylabel('mae')       
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+#plt.savefig("Keras_NN_Accuracy.png")
+plt.show()
+plt.clf()
+    
 #print(trainbst.evals_result())
 print("####################KNN")
 neigh = KNeighborsRegressor(n_neighbors=10,weights='distance',n_jobs=-1)
+valiN = cross_val_score(neigh,Xvalid,Yvalid,cv=kf,verbose=1,n_jobs=-1)
+print(valiN.mean())
 neigh.fit(Xtrain,Ytrain)
 print(neigh.get_params())
 print(neigh.score(Xvalid,Yvalid))
+# plt.plot(list(evres['validation_0']['rmse']))
+# plt.plot(list(evres['validation_1']['rmse']))
+# plt.title('Model rmse')
+# plt.ylabel('rmse')       
+# plt.xlabel('Epoch')
+# plt.legend(['Train', 'Test'], loc='upper left')
+# #plt.savefig("Keras_NN_Accuracy.png")
+# plt.show() 
+# plt.clf()
 """
 print("##################SVM")
 s=SVR(verbose=True)
